9f816f4de115dcb9621183acb6ac6ad99227d73d
Yuying <yycchen0327@gmail.com>
2023-06-24 21:38:24 +0800

    變數宣告

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,11 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    // var 定義可改變值的變數。
+    var userName: String = "Yuying Chen"
+    // val 定義不可改變值的變數，另外可以不聲明類型，會自行推斷
+    val age = 30
+    // age = 31 // Val cannot be reassigned
+    // age = "30" // Type mismatch: inferred type is String but Int was expected
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    // 使用$符號來串接字串
+    println("Hello $userName is now $age years old.")
 }
